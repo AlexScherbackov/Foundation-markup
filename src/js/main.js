54947c40e1214f819a2101258ec95dabfa9b2595
AlexScherbakov <alsherbakov13@gmail.com>
2018-02-01 08:46:14 +0300

    first commit & top-line

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,12 @@
-"use strict";
 svg4everybody();
 
 // @include('detect.js')
 // @include('globals.js')
-// @include('menudate.js')
-// @include('functions.js')
-
-// @include('./classes/menu.js')
 
 
-
-	const menu = new Megamenu(menuData);
-
-	const menuItems = $document[0].querySelectorAll('.main-head-menu__link');
-	console.log(menuItems);
-
-	menuItems.forEach( function(element, index) {
-		element.addEventListener('click', ()=>{
-			const menuBlock = element.parentNode.querySelector('.megamenu');
-			element.classList.toggle('open');
-			menuBlock.classList.toggle('open')
-		});
-	});		
-
-
-
-
+$(document).ready(function(){
+	$('#mobile-menu-btn').click(function(){
+		$(this).toggleClass('open');
+		$('#mobile-menu').toggleClass('open');
+	});
+});
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -2,5 +2,3 @@
 // @include('../../node_modules/bowser/bowser.js')
 // @include('../../node_modules/svg4everybody/dist/svg4everybody.js')
 // @include('../../node_modules/jquery/dist/jquery.js')
-// @include('../../bower_components/magnific-popup/dist/jquery.magnific-popup.js')
-// @include('../../bower_components/slick-carousel/slick/slick.js')
